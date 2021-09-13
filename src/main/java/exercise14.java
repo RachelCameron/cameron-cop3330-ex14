db7d9eb7634c94e8b69938db345f056fc4148824
Rachel Cameron <rachleecameron@gmail.com>
2021-09-13 15:35:13 -0400

    Initial commit

---
+++ src/main/java/exercise14.java
@@ -0,0 +1,23 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Rachel Cameron
+ */
+
+import java.util.Scanner;
+
+public class exercise14 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is the order amount?");
+        double orderamount = input.nextDouble();
+        System.out.println("What is the state?");
+        String state = input.next();
+        double tax = .055;
+        if(state.equalsIgnoreCase("WI")){
+            System.out.printf("The subtotal is $%.2f\n", orderamount);
+            System.out.printf("The tax is $%.2f\n", tax);
+        }
+        double total = orderamount + tax;
+        System.out.printf("The total is $%.2f\n", total);
+    }
+}
